b0dfac9e2561d57820f63ea153e0b5992316a008
kishore.chinna <kishore.c@accubits.com>
2025-02-22 12:37:05 +0530

    disable checks

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off', // Disable `any` check
+    '@typescript-eslint/no-unused-vars': 'off', // Allow unused variables
+    'react-hooks/exhaustive-deps': 'off', // Ignore `useEffect` missing dependencies
+  },
+};
